90b2cfde7719a1c2a55df2800c5e9be86370ca17
Samara Cassie <samaracassie@outlook.com>
2023-03-07 14:24:51 -0300

    novos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NovoComponenteComponent } from './paginas/novo-componente/novo-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovoComponenteComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/paginas/novo-componente/novo-componente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-componente',
+  templateUrl: './novo-componente.component.html',
+  styleUrls: ['./novo-componente.component.css']
+})
+export class NovoComponenteComponent {
+
+}
