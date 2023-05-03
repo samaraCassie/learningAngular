cc6280094dc00591e6bd1d8d4dbe18c3af447a4d
samaraCassie <samaracassie@outlook.com>
2023-05-02 21:07:27 -0300

    ContribuicaoSolidaria

---
+++ Contribuicao_solidaria/src/app/app-input/app-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-input',
+  templateUrl: './app-input.component.html',
+  styleUrls: ['./app-input.component.css']
+})
+export class AppInputComponent {
+
+}
